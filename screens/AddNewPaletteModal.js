ea253484af108dce749d1d7d0522b9bd78a942d0
Shannon Beach <shannonbeach1982@gmail.com>
2020-09-03 08:50:09 -0400

    added name input and submit button for new palette

--- screens/AddNewPaletteModal.js
+++ screens/AddNewPaletteModal.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
 const AddNewPaletteModal = () => {
+  const [name, setName] = useState('');
+  const handleSubmit = useCallback(() => {
+    if (!name) {
+      Alert.alert('You must enter a name');
+    }
+  }, [name]);
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>Name of the palette</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Palette Name"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  name: {
+    padding: 5,
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 15,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default AddNewPaletteModal;
